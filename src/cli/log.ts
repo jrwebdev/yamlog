7066fa2267b5b2774f0d148c8482cb30fb21d5f0
jrwebdev <web@jravenscroft.co.uk>
2017-10-17 20:37:03 +1300

    Log prompt

--- src/cli/log.ts
+++ src/cli/log.ts
@@ -1,3 +1,14 @@
+import prompt from '../util/prompt';
 import log from '../lib/log';
 
 log('breaking', 'test change 123');
+
+const run = async () => {
+  const answers = await prompt();
+  log(answers.type, answers.details);
+  if (answers.logAnother) {
+    run();
+  }
+};
+
+run();
---
+++ src/util/prompt.ts
@@ -0,0 +1,39 @@
+import * as inquirer from 'inquirer';
+
+const getDefaultType = (message: string) =>
+  /(^| )fix( |$)/i.test(message) ? 'fix' : 'feature';
+
+// TODO: Validation
+
+const prompt = ({ defaultMessage = '', defaultModule = '' } = {}) => {
+  const inquirerPrompt = inquirer.createPromptModule();
+  return inquirerPrompt([
+    {
+      type: 'list',
+      name: 'type',
+      message: 'Which type of change do you want to log?',
+      choices: ['fix', 'feature', 'breaking'],
+      default: getDefaultType(defaultMessage),
+    },
+    {
+      type: 'input',
+      name: 'module',
+      message: 'Which module does this change affect?',
+      default: defaultModule || undefined,
+    },
+    {
+      type: 'input',
+      name: 'details',
+      message: 'Change details',
+      default: defaultMessage || undefined,
+    },
+    {
+      type: 'confirm',
+      name: 'logAnother',
+      message: 'Log another change?',
+      default: false,
+    },
+  ]);
+};
+
+export default prompt;
