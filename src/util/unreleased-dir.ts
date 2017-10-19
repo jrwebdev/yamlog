02edec7899ddc6a467c2ab2dcd888b4b32e1316a
jrwebdev <web@jravenscroft.co.uk>
2017-10-19 22:37:28 +1300

    Refactoring

--- src/util/unreleased-dir.ts
+++ src/util/unreleased-dir.ts
@@ -1,11 +1,20 @@
 import * as moment from 'moment';
+import * as cbGlob from 'glob';
+import * as pify from 'pify';
 import { mkdirs } from 'fs-extra';
 
-import { ChangeType, Change } from '../types/changelog';
-import { write as writeYamlFile } from './yaml-file';
+import { ChangeType, Change, ChangelogVersion } from '../types/changelog';
+import { write as writeYamlFile, read as readYamlFile } from './yaml-file';
 import { processChange } from './changelog-helpers';
 
 const defaultDir = '.yamlog-unreleased';
+
+const glob = pify(cbGlob);
+
+const getType = (filename: string) => {
+  const match = filename.match(/(breaking|feature|fix)-[0-9]{17}\.yaml/);
+  return match ? match[1] as ChangeType : undefined;
+};
 
 export const addChange = async (
   type: ChangeType,
@@ -15,4 +24,21 @@ export const addChange = async (
   await mkdirs(dir);
   const filename = `${type}-${moment().format('YYYYMMDDHHmmssSSS')}.yaml`;
   return writeYamlFile(`${dir}/${filename}`, processChange(change));
+};
+
+export const read = async (_dir = defaultDir) => {
+  const changes: ChangelogVersion = {};
+  const files: string[] = await glob('+(breaking|feature|fix)-*.yaml');
+  if (files && files.length) {
+    files.forEach(async file => {
+      const type = getType(file);
+      if (type) {
+        const change = (await readYamlFile(file)) as Change;
+        // TODO: Clean up ordering etc.
+        changes[type] = [...(changes[type] || []), change];
+      }
+    });
+  }
+
+  return changes;
 };
