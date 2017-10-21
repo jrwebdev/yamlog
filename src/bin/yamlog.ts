a91e0f91e55c9932c2684bf027b15151a7199e02
jrwebdev <web@jravenscroft.co.uk>
2017-10-22 09:45:04 +1300

    Experimentation using commander

--- src/bin/yamlog.ts
+++ src/bin/yamlog.ts
@@ -3,6 +3,18 @@ import * as path from 'path';
 import { argv } from 'yargs';
 import { existsSync } from 'fs-extra';
 
+// TODO: Use commander http://slides.com/timsanteford/conquering-commander-js
+// import * as program from 'commander';
+
+// program
+//   .command('log', 'Logs a change to the changelog')
+//   .command('bump-version', 'Bumps the version if there are unreleased changes')
+//   .command('changes', 'Outputs changes to the console')
+//   .action(env => {
+//     console.log(env);
+//   })
+//   .parse(process.argv);
+
 const command = argv._[0] || 'log';
 const commandFile = path.resolve(__dirname, `../cli/${command}`);
 
