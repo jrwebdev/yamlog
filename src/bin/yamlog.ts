7b110fd7b171aaf6b1f2b4029612e7164cbf6cbd
jrwebdev <web@jravenscroft.co.uk>
2017-10-21 15:23:34 +1300

    Build process

--- src/bin/yamlog.ts
+++ src/bin/yamlog.ts
@@ -1,7 +1,14 @@
 #!/usr/bin/env node
+import * as path from 'path';
 import { argv } from 'yargs';
+import { existsSync } from 'fs-extra';
 
-const script = argv._[0] || 'log';
+const command = argv._[0] || 'log';
+const commandFile = path.resolve(__dirname, `../cli/${command}`);
 
-// TODO: Check script exists
-require(`../cli/${script}`);
+if (existsSync(commandFile)) {
+  require(commandFile);
+} else {
+  // TODO: Output help
+  console.error(`${command} is not a valid yamlog command.`);
+}
